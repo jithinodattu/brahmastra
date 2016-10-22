1afefb236f69fa7b6f6f4fa089b17686f71f79be
jithinoc <jithindotcom@gmail.com>
2016-10-22 20:33:08 +0530

    Added activation_function, weight_initializer params in dense_layer

--- source/layers/dense_layer.py
+++ source/layers/dense_layer.py
@@ -7,28 +7,37 @@ from layer import Layer
 
 class DenseLayer(Layer):
 
-	def __init__(self, n_in, n_out):
+	def __init__(self, n_in, n_out, activ_fn=None, w_initializer=None):
 		super(DenseLayer, self).__init__()
 
-		self.W = theano.shared(
-			value=numpy.zeros(
+		if w_initializer:
+			W_value = w_initializer(n_in, n_out, activ_fn)
+		else:
+			W_value = numpy.zeros(
 				(n_in, n_out),
 				dtype=theano.config.floatX
-				),
+				)
+
+
+		self.W = theano.shared(
+			value=W_value,
 			name='W',
 			borrow=True
 			)
 
+		b_values = np.zeros((n_out,), dtype=theano.config.floatX)
 		self.b = theano.shared(
-			value=numpy.zeros(
-				(n_out,),
-				dtype=theano.config.floatX
-				),
+			value=b_values,
 			name='b',
 			borrow=True
 			)
 
+		self.activ_fn = activ_fn
+
 		self.params = [self.W, self.b]
 
 	def feedforward(self, input):
-		self.output = T.dot(input, self.W) + self.b
+		if self.activ_fn:
+			self.output = self.activ_fn(T.dot(input, self.W) + self.b)
+		else:
+			self.output = T.dot(input, self.W) + self.b
