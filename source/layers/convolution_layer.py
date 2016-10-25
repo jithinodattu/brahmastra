0b65dda74e73ba3170e049fe9a2ab05aefeb0100
jithinoc <jithindotcom@gmail.com>
2016-10-25 21:36:10 +0530

    Created convolution_layer and implemented 2D-conv

---
+++ source/layers/convolution_layer.py
@@ -0,0 +1,49 @@
+
+import numpy
+import theano
+import theano.tensor as T 
+from theano.tensor.nnet import conv2d
+
+from layer import Layer
+from ..utils.weight_initializer import gloret
+
+class Convolution2DLayer(Layer):
+
+	def __init__(self, filter_shape, image_shape, activ_fn=None, w_initializer=gloret):
+
+		if w_initializer:
+			W_value = w_initializer(filter_shape[0], filter_shape[1], activ_fn)
+		else:
+			W_value = numpy.zeros(
+				(filter_shape[0], filter_shape[1]),
+				dtype=theano.config.floatX
+				)
+
+		self.W = theano.shared(
+			value=W_value,
+			name='W'
+			borrow=True
+			)
+
+		b_values = numpy.zeros((filter_shape[0],), dtype=theano.config.floatX)
+		self.b = theano.shared(value=b_values, borrow=True)
+
+		self.filter_shape = filter_shape
+		self.image_shape = image_shape
+		self.activ_fn = activ_fn
+
+		self.params = [self.W, self.b]
+
+	def feedforward(self, input):
+		conv_out = conv2d(
+			input=input,
+			filters=self.W,
+			filter_shape=self.filter_shape,
+			input_shape=self.image_shape
+			)
+
+		output = conv_out + self.b.dimshuffle('x', 0, 'x', 'x')
+		if self.activ_fn:
+			self.output =activ_fn(output)
+		else:
+			self.output = output
